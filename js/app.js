f98ad56051a56e98875646c7d22dae9686cce15a
Tomohiro Meo <h2.adachi.tomohiro@gmail.com>
2021-02-14 21:43:24 -0800

    Delete code

--- js/app.js
+++ js/app.js
@@ -109,37 +109,35 @@ document.querySelector(".item-total").textContent = finalMoney;
 document.getElementById("item-count").textContent = total;
 
 
-let items = JSON.parse(localStorage.getItem("items"))
+const items = JSON.parse(localStorage.getItem("items"))
 
-if (items) {
-  for (var i = 0; i < items.length; i++) {
-    const cartItem = document.createElement("div");
-    cartItem.classList.add(
-      'cart-item',
-      'd-flex',
-      'justify-content-between',
-      'text-capitalize',
-      'my-3'
-    );
 
-    cartItem.innerHTML = `
-      <img src="${items[i].img}" class="img-fluid rounded-circle" id="item-img" alt="">
-      <div class="item-text">
-        <p id="cart-item-title" class="font-weight-bold mb-0">${items[i].name}</p>
-        <span>$</span>
-        <span id="cart-item-price" class="cart-item-price" class="mb-0">${items[i].price}</span>
-      </div>
-      <a href="#" id='cart-item-remove' class="cart-item-remove">
-        <i class="fas fa-trash"></i>
-      </a>
+for (var i = 0; i < items.length; i++) {
+  const cartItem = document.createElement("div");
+  cartItem.classList.add(
+    'cart-item',
+    'd-flex',
+    'justify-content-between',
+    'text-capitalize',
+    'my-3'
+  );
+
+  cartItem.innerHTML = `
+    <img src="${items[i].img}" class="img-fluid rounded-circle" id="item-img" alt="">
+    <div class="item-text">
+      <p id="cart-item-title" class="font-weight-bold mb-0">${items[i].name}</p>
+      <span>$</span>
+      <span id="cart-item-price" class="cart-item-price" class="mb-0">${items[i].price}</span>
     </div>
-  `;
-    const cart = document.getElementById("cart");
-    const total = document.querySelector(".cart-total-container");
-    cart.insertBefore(cartItem, total);
-    removeItem()
-  }
-  localStorage.setItem("items", JSON.stringify(save_items));
+    <a href="#" id='cart-item-remove' class="cart-item-remove">
+      <i class="fas fa-trash"></i>
+    </a>
+  </div>
+`;
+  const cart = document.getElementById("cart");
+  const total = document.querySelector(".cart-total-container");
+  cart.insertBefore(cartItem, total);
+  removeItem()
 }
 
 function removeItem() {
